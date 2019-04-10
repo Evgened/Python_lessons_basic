9c6632351d5c84970599b559f296c179240f4291
Evgened <49209884+Evgened@users.noreply.github.com>
2019-04-10 22:58:04 +0300

    Lesson3, easy

--- lesson03/home_work/hw03_easy.py
+++ lesson03/home_work/hw03_easy.py
@@ -5,13 +5,19 @@
 # Для решения задачи не используйте встроенные функции и функции из модуля math.
 
 def my_round(number, ndigits):
-    pass
+    power = 10**ndigits
+    rest = (number * power % 1) / power
+    trunc_num = number - rest
+    if rest * power < 0.5:
+        return number - rest
+    else:
+        return trunc_num + 1/power
 
 
 print(my_round(2.1234567, 5))
 print(my_round(2.1999967, 5))
 print(my_round(2.9999967, 5))
-
+print(my_round(2.0000047, 5))
 
 # Задание-2:
 # Дан шестизначный номер билета. Определить, является ли билет счастливым.
@@ -20,7 +26,22 @@ print(my_round(2.9999967, 5))
 # !!!P.S.: функция не должна НИЧЕГО print'ить
 
 def lucky_ticket(ticket_number):
-    pass
+    v_ticket=list(str(ticket_number))
+    v_len=len(v_ticket)
+    v_left_side=0
+    v_right_side=0
+    if v_len==6:
+        for i in range(6):
+            if i <= 3:
+                v_left_side += int(v_ticket[i])
+            else:
+                v_right_side += int(v_ticket[i])
+        if v_left_side == v_right_side:
+            return 'Yes'
+        else:
+            return 'No'
+    else:
+        return 'No'
 
 
 print(lucky_ticket(123006))
