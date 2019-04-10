b7b5cee42e52307138c9b633bf5330bf5a80cfba
Evgened <49209884+Evgened@users.noreply.github.com>
2019-04-11 01:07:05 +0300

    normal, решение задач 1-2

--- lesson03/home_work/hw03_normal.py
+++ lesson03/home_work/hw03_normal.py
@@ -2,19 +2,51 @@
 # Напишите функцию, возвращающую ряд Фибоначчи с n-элемента до m-элемента.
 # Первыми элементами ряда считать цифры 1 1
 
-def fibonacci(n, m):
-    pass
+def fibonacci(n,m):
+
+    list_fib = []
+    j = 0
+    def n_fibonacci (x):
+        if x == 0:
+            return 0
+        elif x == 1:
+            return 1
+        else:
+            return n_fibonacci (x-1) + n_fibonacci (x-2)
+
+    for i in range(n,m+1):
+        j += 1;
+        list_fib.append(n_fibonacci(i))
+
+    return list_fib
+
+# print (fibonacci(10,20))
+
 
 # Задача-2:
 # Напишите функцию, сортирующую принимаемый список по возрастанию.
 # Для сортировки используйте любой алгоритм (например пузырьковый).
 # Для решения данной задачи нельзя использовать встроенную функцию и метод sort()
 
-
 def sort_to_max(origin_list):
-    pass
+    # пузырёк так пузырёк
+    v_origin_list = list(origin_list)
+    v_sort_list = []
+    list_len = len(v_origin_list)
+    v_min = float(0)
+    v_min_pos = 0
 
-sort_to_max([2, 10, -12, 2.5, 20, -11, 4, 4, 0])
+    for i in range(list_len):
+        v_min = float(v_origin_list[0])
+        v_min_pos = 0
+        for j in range(list_len - i):
+            if float(v_origin_list[j]) < v_min:
+                v_min = float(v_origin_list[j])
+                v_min_pos = j
+        v_sort_list.append(v_origin_list.pop(v_min_pos))
+    return v_sort_list
+
+print(sort_to_max([2, 10, -12, 2.5, 20, -11, 4, 4, 0]))
 
 # Задача-3:
 # Напишите собственную реализацию стандартной функции filter.
