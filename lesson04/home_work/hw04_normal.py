e4ce49aa51449ad04c0d5a3ed58d56c2bfea86da
evgened <evgened@outlook.com>
2019-04-15 15:42:21 +0300

    Lesson4 Easy - решение норм Normal - решение примитивно :(

--- lesson04/home_work/hw04_normal.py
+++ lesson04/home_work/hw04_normal.py
@@ -4,6 +4,8 @@
 # Т.е. из строки "mtMmEZUOmcq" нужно получить ['mt', 'm', 'mcq']
 # Решить задачу двумя способами: с помощью re и без.
 
+import re
+
 line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'GIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewVzK'\
        'TUfSYtBydXaVIpxWjNKgXANvIoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVjfSAHqn'\
@@ -20,11 +22,25 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'XiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQ'\
        'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
 
+temp_line = line
+line_not_re = []
+
+for i in ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']:
+       temp_line = temp_line.replace(i, ' ')
+
+line_not_re = temp_line.split(' ')
+# убиаем пустые
+line_not_re = [i for i in line_not_re if i]
+
+print (line_not_re)
+
+line_re = re.findall(r'[a-z]+', line)
+print (line_re)
 
 # Задание-2:
 # Вывести символы в верхнем регистре, слева от которых находятся
 # два символа в нижнем регистре, а справа - два символа в верхнем регистре.
-# Т.е. из строки 
+# Т.е. из строки
 # "GAMkgAYEOmHBSQsSUHKvSfbmxULaysmNOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLec"
 # нужно получить список строк: ['AY', 'NOGI', 'P']
 # Решить задачу двумя способами: с помощью re и без.
@@ -45,9 +61,66 @@ line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ'\
        'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
 
+
+temp_line = line_2
+line_not_re = []
+for i,x in enumerate(line_2):
+       if x == x.upper() and line_2[i-2:i] == line_2[i-2:i].lower() and line_2[i+1:i+3] == line_2[i+1:i+3].upper():
+              # первая буква
+              last_upper = i+1
+              for j,y in enumerate(line_2[i+1:]):
+                     if y == y.upper():
+                            last_upper = j+i+1
+                     else:
+                          break
+              if line_2[i:last_upper-1] != '':
+                     line_not_re.append(line_2[i:last_upper-1])
+
+# способ 2
+line_re = re.findall(r'[a-z]{2}[A-Z]{1,}[A-Z]{2,}', temp_line)
+line_re = [i[2:-2] for i in line_re]
+
+print (line_re)
+print (line_not_re)
+
+
 # Задание-3:
 # Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
 # произвольными целыми цифрами, в результате в файле должно быть
 # 2500-значное произвольное число.
 # Найдите и выведите самую длинную последовательность одинаковых цифр
 # в вышезаполненном файле.
+
+filename = 'randint.txt'
+import os, random
+n = 2500
+rand_lim = 9
+
+with open(filename, 'w', encoding='UTF-8') as f:
+       for i in range(n):
+              f.write(str(random.randint(0,rand_lim)))
+
+with open(filename, 'r', encoding='UTF-8') as f:
+       longint = f.readline(n)
+
+print (longint)
+
+long_pos = 0
+long_pos_value = longint[0:1]
+max_counter = 0
+current_counter = 1
+prev_value = ''
+for i, x in enumerate(list(longint)):
+       if x == long_pos_value and x == prev_value:
+              current_counter += 1
+              if current_counter > max_counter:
+                     long_pos = i
+                     max_counter +=1
+                     long_pos_value = str(x)
+       else:
+            current_counter = 1
+       prev_value = x
+
+print (long_pos_value, '; position=', long_pos, '; repeat: ', max_counter)
+print (longint[long_pos-max_counter+1:long_pos+1])
+
